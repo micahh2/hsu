d316dab5ce8143eac615f7b4fbbf11aa8028b8ce
Micah D <micah@dvyld.com>
2020-10-24 14:42:02 +0200

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+echo "Syncing $1 to $2"
+rsync -r --exclude='.git' \
+      --exclude='.gitignore' \
+      --exclude='node_modules' \
+      --exclude='package*.json' \
+      --exclude='*.lock' \
+      --exclude='*.spec.*' \
+      --exclude='*.sh' \
+      $1 $2
