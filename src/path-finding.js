0b8a9967db4cc6cbf2ac2d59cbc52ac808dd95dc
antoniooreany <antonio_oreany@mail.ru>
2020-11-14 15:45:41 +0100

    the logic was implemented, tests are passed, TODO*2 created

--- src/path-finding.js
+++ src/path-finding.js
@@ -1,71 +1,88 @@
-export class PathFinding {
-  static star({ graph, start, finish }) {
-    const next = [];
-    const place = start;
-    // TODO: Add case where there is no solution
-    while (place.x === finish.x && place.y === finish.y) {
+/**
+* PathFinding.
+* Module for path finding function
+*/
+export const PathFinding = {
+  /**
+  * aStar.
+  * A* Path Finding
+  */
+  aStar({graph, start, finish}) {
+    const nextPlaces = [];
+    let place = start;
+    while (place && (place.x !== finish.x || place.y !== finish.y)) {
       const maxNumberOfNeighbors = 9;
-      // eslint-disable-next-line no-plusplus
+      // eslint-disable-nextPlaces-line no-plusplus
       for (let i = 0; i < maxNumberOfNeighbors; i++) {
-        const offsgietX = 1 - (i % 3);// -1,  0,  1, -1, 0, 1, -1, 0, 1
+        const offsetX = 1 - (i % 3);// -1,  0,  1, -1, 0, 1, -1, 0, 1
         const offsetY = 1 - Math.floor(i / 3);// -1, -1, -1,  0, 0, 0,  1, 1, 1
         const newX = place.x + offsetX;
         const newY = place.y + offsetY;
         // Don't stay in the same place
-        if (offsetX === 0 && offsetY === 0) {
-          return;
-        }
-
+        if (offsetX === 0 && offsetY === 0) continue;
         // Bounds of the graph could not be going through
-        if (newX < 0 || newY < 0) {
-          return;
-        }
-        if (newY >= graph.length || newX >= graph[newY].length) {
-          return;
-        }
+        if (newX < 0 || newY < 0) continue;
+
+        if (newY >= graph.length || newX >= graph[newY].length) continue;
         // Don't go in a cycle/loop
-        if (next.some((t) => t.x === newX && t.y === newY)) {
-          return;
-        }
+        if (nextPlaces.some((place) => place.x === newX && place.y === newY)) continue;
+
+        if (graph[newY][newX] > 0) continue;
+
         const newPlace = {
           x: newX,
           y: newY,
           from: place,
         };
-        // TODO: define newPlace.cost based on:
-        // last cost + distance cost from place + distance cost to finish
+        newPlace.cost = (place.cost || 0)
+          + PathFinding.distanceCost(place, newPlace);
+          + PathFinding.distanceCost(newPlace, finish);
 
-        next.push(newPlace);
+        nextPlaces.push(newPlace);
       }
-      // TODO: Sort next with cost
-      // TODO: Update place with next.shift
+      // Sort ascending order
+      nextPlaces.sort((place1, place2) => place1.cost - place2.cost);
+      // Remove the first element of array
+      place = nextPlaces.shift();
     }
-    // TODO: "place" is now the destination (plus some more)
-    // TODO: define a variable to hold last direction
-    // TODO: go backwards through the path using the "from"s that you setup before:
-    // TODO: while place is not undefined "while(place)"
-    // TODO: if the direction has changes add the place to the path
-    // TODO: update the place with it's "from" variable
-    // after the path, reverse the path before returning it
-    return []; // TODO: return the real path as a Array of changes
-  }
+    // "place" is now the destination (and it has a "from" property), or undefined
+    let lastDirection = undefined;
+    const path = [];
+    // Go backwards through the path using the "from"s that setup before:
+    while (place && place.from) {
+      const newDirection = PathFinding.getDirection(place, place.from);
+      if (lastDirection !== newDirection) {
+        lastDirection = newDirection;
+        path.push({x: place.x, y: place.y});
+      }
+      place = place.from;
+    }
+    // reverse the path before returning it
+    path.reverse();
+    return path;
+  },
 
-  // Returns a string that is 0:0, -1:0, 1:
-  static getDirection(a, b) {
-    if (!b) {
+  /**
+  * getDirection.
+  * A* Path Finding
+  * @returns {string} something like: '0:0', '-1:0', '1:1', 'origin'
+  */
+  getDirection: function (start, finish) {
+    if (finish) {
+      return `${Math.sign(start.x - finish.x)}:${Math.sign(start.y - finish.y)}`;
+    } else {
       return 'origin';
     }
-    return `${Math.sign(a.x - b.x)}:${Math.sign(a.y - b.y)}`;
-  }
+  },
 
-  // The distance if you can't go diagonally
-  static distanceCost(a, b) {
-    // if (a.x === newPlace.x) {
-    //   return Math.sqrt(2) * (a.y - a.y);
-    // } // TODO: extend it for diagonal moving
-    // if (a.y === newPlace.y) {
-    //   return Math.sqrt(2) * (a.x - b.x);
-    // } // TODO: extend it for diagonal moving
-    return Math.abs(a.x - b.x) + Math.abs(a.y - a.y);
+  /**
+   * distanceCost.
+   * @param start
+   * @param finish
+   * @returns {number} - distance between start a finish
+   */
+  distanceCost(start, finish) {
+    return Math.abs(start.x - finish.x)
+      + Math.abs(start.y - finish.y);
   }
-}
+};
