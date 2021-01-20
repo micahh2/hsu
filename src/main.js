f9f4b21eeb6023d98ba7fbd5ff385d4a78ed51dd
Eetu Hyvärinen <eetu_hyvarinen@hotmail.com>
2021-01-20 01:31:38 +0100

    fixed an error with exposure bar going full if exposure level is NaN

--- src/main.js
+++ src/main.js
@@ -283,15 +283,22 @@ window.addEventListener('load', async () => {
       Music.playTrack('#waterdrop', false);
       lastDrop = new Date();
     }
+    // Update exposure bar
     if (oldState.player.exposureLevel !== physicsState.player.exposureLevel) {
-    document.getElementById('progress-bar').setAttribute('value', physicsState.player.exposureLevel);
+      if (isNaN(physicsState.player.exposureLevel)) {
+        document.getElementById('progress-bar').setAttribute('value', 0);
+    } else {
+      document.getElementById('progress-bar').setAttribute('value', physicsState.player.exposureLevel);
     }
+  }
+    
     // Show end screen if it's the end
     if (physicsState.end) {
       alert('Bummer'); // eslint-disable-line no-alert
       window.location = window.location; // eslint-disable-line no-self-assign
       return;
     }
+    
     // Show/update conversation if that changes
     if (oldState.conversation !== physicsState.conversation) {
       const updateConvo = (newConvo) => {
@@ -702,7 +709,7 @@ function renderMessageOverlay(element) {
 let playing = false;
 function playMusic() {
   if (playing) { return; }
-  Music.playTrack('#backgroundchill', false).catch(() => { playing = false; });
+  Music.playTrack('#backgroundchill', true).catch(() => { playing = false; });
   playing = true;
 }
 // updates the volume based on where the volumeslider is
